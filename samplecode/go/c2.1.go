f589a68125b41105e5787369ba4abd0e135882ac
juxu <falcon.xu@montage-lz.com>
2019-04-26 18:07:54 +0800

    udpate go code

--- samplecode/go/c2.1.go
+++ samplecode/go/c2.1.go
@@ -12,8 +12,10 @@ d. 二进制[1001101110011110110101]转换为十六进制
 func C2_1() {
 	const hexa int = 0x39A7F8
 	fmt.Printf("dex:%x bin:%b\n", hexa, hexa)
-}
 
+	const binb string = "1100100101111011"
+	//实现不写了,麻烦:(
+}
 
 /*
 练习题2.4 实现以下c代码
@@ -48,7 +50,21 @@ show_float(fval);
 show_pointer(pval);
 
 */
+type bytePointer = byte
 
+func show_bytes(start *bytePointer, len int) {
+	for i := 0; i < len; i++ {
+		fmt.Printf("%.2x", start+i)
+	}
+	fmt.Printf("\n")
+}
+
+func C2_4() {
+	const hexa int = 0x39A7F8
+	fmt.Printf("dex:%x bin:%b\n", hexa, hexa)
+
+	const binb string = "1100100101111011"
+}
 
 /*
 练习题2.5 思考下面对show_bytes的三次调用
@@ -62,14 +78,12 @@ show_bytes(pval, 3);
 在大端和小端机器上,输出分别是什么?
 */
 
-
 /*
 练习题2.7 下面对show_bytes的调用将输出什么结果
 
 const char *s = "abcdef";
-show_bytes((byte_pointer)s, strlen(s)); 
+show_bytes((byte_pointer)s, strlen(s));
 */
-
 
 /*
 练习题2.10 对任一向量a 有 a^a=0, 应用这一属性, 考虑以下程序:
@@ -81,7 +95,6 @@ void inplace_swap(int *x, iny *y){
 }
 */
 
-
 /*
 练习题2.11 基于2.10的函数, 实现数组元素头尾对调
 
@@ -90,9 +103,8 @@ void reverse_array(int a[], int cnt){
 }
 */
 
-
 /*
-练习题2.13 
+练习题2.13
 考虑没有布尔运算符 AND 和 OR 指令的机器中, 使用
 
 int bis(int x, int m): 返回在m为1的每个位上,设置x对应位为1的值
@@ -108,4 +120,3 @@ int bool_xor(int x, int y){
 	return ____;
 }
 */
-
